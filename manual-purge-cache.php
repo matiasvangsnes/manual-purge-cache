71768103a6991b2f5ac7c78866fe0937840975a9
phpanos <phpanos@gmail.com>
2014-12-03 12:16:48 +0100

    wrapped the purge cache functionality into a function

--- manual-purge-cache.php
+++ manual-purge-cache.php
@@ -1,64 +1,15 @@
 <?php
 
 // Configuration
-$w3tc_location = "wp-content/cache/page_enhanced/";
-$supercache_location = "wp-content/cache/supercache/";
+define('W3TC_LOCATION', 'wp-content/cache/page_enhanced/');
+define('SUPERCACHE_LOCATION', 'wp-content/cache/supercache/');
 $rawurl = $_POST["mpc-url"];
-$wpurl = site_url();
-$parsedwpurl = parse_url($wpurl);
 
 if (isset($_POST["mpc-url"])) {
 ?>
 <div id="message" class="updated">
 <?php
-	if (strpos($rawurl, "http") === 0) {
-		$parsedrawurl = parse_url($rawurl);
-		$parsedwpurl = parse_url($wpurl);
-		if ($parsedwpurl[host] == $parsedrawurl[host]) {
-			$purgepath = ABSPATH . $w3tc_location . $parsedwpurl[host] . $parsedrawurl[path] . "/";
-			if (file_exists($purgepath."_index.html")) {
-				if (!unlink($purgepath."_index.html")) {
-					echo ("<p><strong>Fail!</strong> Error deleting _index.html.");
-				}
-				else {
-					echo ("<p><strong>Hepp!</strong> The cached file _index.html is deleted.</p>");
-				}
-			}
-			elseif (file_exists($purgepath."_index.html_gzip")) {
-      	if (!unlink($purgepath."_index.html_gzip")) {
-        	echo ("<p><strong>Fail!</strong> Error deleting _index.html_gzip.</p>");
-      	}
-      	else {
-      	echo ("<p><strong>Hepp!</strong> The cached file _index.html_gzip is deleted.");
-      	}
-			}
-      elseif (file_exists($purgepath."_index.html.old")) {
-      	if (!unlink($purgepath."_index.html.old")) {
-        	echo ("<p><strong>Fail!</strong> Error deleting _index.html.old.</p>");
-        }
-        else {
-        	echo ("<p><strong>Hepp!</strong> The cached file _index.html.old is deleted.</p>");
-        }
-			}
-      elseif (file_exists($purgepath."_index.html_gzip.old")) {
-      	if (!unlink($purgepath."_index.html_gzip.old")) {
-        	echo ("<p><strong>Fail!</strong> Error deleting _index.html_gzip.old.</p>");
-        }
-        else {
-        	echo ("<p><strong>Hepp!</strong> The cached file _index.html_gzip.old is deleted.</p>");
-        }
-      }
-      else {
-      	echo "<p><strong>Fail!</strong> There is no cached files in the folder.</p>";
-      }
-		}
-		else {
-			echo "<p><strong>Fail!</strong> The posted URL domain is not matching your WordPress installation domain.</p>";
-		}
-	}
-	else {
-		echo "<p><strong>Fail!</strong> You must enter a correct URL which begin with http or https (example: http://www.example.com/folder/?id=232323)</p>";
-	}
+	mpc_purge_cache($rawurl);
 ?>
 </div>
 <?php
@@ -67,10 +18,10 @@ if (isset($_POST["mpc-url"])) {
 <div class="wrap">
 	<h2><?php echo __("Manual purge cache", "mpc") ?></h2>
 <?php
-if (file_exists(ABSPATH . $w3tc_location)) {
+if (file_exists(ABSPATH . W3TC_LOCATION)) {
 		echo ("<p>You are using W3 Total Cache.</p>");
 }
-elseif (file_exists(ABSPATH . $supercache_location)) {
+elseif (file_exists(ABSPATH . SUPERCACHE_LOCATION)) {
 		echo ("<p>You are using WP Super Cache.</p>");
 }
 else {
--- settings.php
+++ settings.php
@@ -14,4 +14,56 @@ function mpc_admin_actions() {
     add_options_page("Manual purge cache", "Manual purge cache", 1, "Manual_purge_cache", "mpc_admin");
 }
 add_action('admin_menu', 'mpc_admin_actions');
+
+function mpc_purge_cache($rawurl) {
+  $wpurl = site_url();
+  $parsedwpurl = parse_url($wpurl);
+  if (strpos($rawurl, "http") === 0) {
+    $parsedrawurl = parse_url($rawurl);
+    $parsedwpurl = parse_url($wpurl);
+    if ($parsedwpurl[host] == $parsedrawurl[host]) {
+      $purgepath = ABSPATH . W3TC_LOCATION . $parsedwpurl[host] . $parsedrawurl[path] . "/";
+
+      if (file_exists($purgepath."_index.html") || file_exists($purgepath."_index.html_gzip")) {
+        if (file_exists($purgepath."_index.html")) {
+          if (!unlink($purgepath."_index.html")) {
+            echo ("<p><strong>Fail!</strong> Error deleting _index.html.");
+          } else {
+            echo ("<p><strong>Hepp!</strong> The cached file _index.html is deleted.</p>");
+          }
+        }
+
+        if (file_exists($purgepath."_index.html_gzip")) {
+          if (!unlink($purgepath."_index.html_gzip")) {
+            echo ("<p><strong>Fail!</strong> Error deleting _index.html_gzip.</p>");
+          } else {
+          echo ("<p><strong>Hepp!</strong> The cached file _index.html_gzip is deleted.");
+          }
+        }
+      } else {
+        if (file_exists($purgepath."_index.html.old")) {
+          if (!unlink($purgepath."_index.html.old")) {
+            echo ("<p><strong>Fail!</strong> Error deleting _index.html.old.</p>");
+          } else {
+            echo ("<p><strong>Hepp!</strong> The cached file _index.html.old is deleted.</p>");
+          }
+        }
+
+        if (file_exists($purgepath."_index.html_gzip.old")) {
+          if (!unlink($purgepath."_index.html_gzip.old")) {
+            echo ("<p><strong>Fail!</strong> Error deleting _index.html_gzip.old.</p>");
+          } else {
+            echo ("<p><strong>Hepp!</strong> The cached file _index.html_gzip.old is deleted.</p>");
+          }
+        } else {
+          echo "<p><strong>Fail!</strong> There is no cached files in the folder.</p>";
+        }
+      }
+    } else {
+      echo "<p><strong>Fail!</strong> The posted URL domain is not matching your WordPress installation domain.</p>";
+    }
+  } else {
+    echo "<p><strong>Fail!</strong> You must enter a correct URL which begin with http or https (example: http://www.example.com/folder/?id=232323)</p>";
+  }
+}
 ?>
